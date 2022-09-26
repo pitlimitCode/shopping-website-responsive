2f5ddd347103c196e288854acfad07bfcb7a70f5
pitlimitCode <prabow14@gmail.com>
2022-09-26 21:21:28 +0700

    [update] home page

--- components/homeThemesCategory.js
+++ components/homeThemesCategory.js
@@ -11,17 +11,37 @@ import "swiper/css/navigation";
 import { Navigation } from "swiper";
 
 function HomeThemes(props){
+  const themes = [
+    { src: "/card/Card Promotion 1.png" },
+    { src: "/card/Card Promotion 2.png" },
+  ];
   return (
     <div className={styles.container}>
       <section className={styles.section}>
-        Themes section, coming soon...
+        <Swiper 
+          className={styles.swiper}
+          slidesPerView={2}
+          centeredSlides={10}
+          spaceBetween={30}
+          navigation={true}
+          loop={true}
+          modules={[Navigation]} 
+        >
+          {themes.map((theme, index) => (
+            <SwiperSlide className='swiper-slide' key={index}>
+              <img
+                className={styles.imgTheme} 
+                src={theme.src}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </section>
     </div>
   )
 }
 
 function HomeCategory(props){
-  
   const categories = [
     { src: "/category/Group 1235.jpg" },
     { src: "/category/Group 1236.jpg" },
@@ -35,14 +55,12 @@ function HomeCategory(props){
     { src: "/category/Group 1244.jpg" },
     { src: "/category/Group 1245.jpg" },
   ];
-
   return (
-    <>
-      <div className={styles.container}>
-        <section className={styles.section}>
-          <div className={styles.title}>Category</div>
-          <div className={styles.desc}>What are you currently looking for</div>
-      
+    <div className={styles.container}>
+      <section className={styles.section}>
+        <div className={styles.title}>Category</div>
+        <div className={styles.desc}>What are you currently looking for</div>
+    
         <Swiper 
           className={styles.swiper}
           slidesPerView={5}
@@ -68,10 +86,8 @@ function HomeCategory(props){
           ))}
         </Swiper>
 
-        </section>
-      </div>
-
-      </>
+      </section>
+    </div>
   )
 }
 
