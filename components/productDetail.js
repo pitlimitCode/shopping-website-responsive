8d696eae74b0d36ddcc138066d1e4e9ee906e57c
pitlimitCode <prabow14@gmail.com>
2022-09-26 20:45:51 +0700

    [update] detail product

--- components/productDetail.js
+++ components/productDetail.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Product.module.css'
 import Products15 from './products15'
@@ -61,9 +61,7 @@ import Products15 from './products15'
 // }
 
 
-
 function ProductDetail(props){
-
   const imgProducts = [
     { src: "/detailProduct/detailproduct5.png" },
     { src: "/detailProduct/detailproduct4.png" },
@@ -71,7 +69,6 @@ function ProductDetail(props){
     { src: "/detailProduct/detailproduct2.png" },
     { src: "/detailProduct/detailproduct1.png" }
   ];
-
   return (
     <div className={styles.container}>
 
@@ -81,7 +78,7 @@ function ProductDetail(props){
         <div className='row'>
 
           <div className='col-4'>
-            <div className='row pb-2'>
+            <div>
               <Image 
                 className={styles.rounded}
                 src={imgProducts[4].src}
@@ -90,7 +87,7 @@ function ProductDetail(props){
                 height='400'
               />
             </div>
-            <div className='d-flex justify-content-between'>
+            <div className='d-flex justify-content-between pt-2'>
               {imgProducts.map((img, index) => (
                 <div key={index}>
                   <Image 
@@ -105,53 +102,60 @@ function ProductDetail(props){
             </div>
           </div>
 
-          <div className='col-8'>
-
+          <div className='col-8 ps-4'>
             <div className={styles.title}>Baju muslim pria</div>
             <div className={styles.h4Grey}>Zabora Cloth</div>
-            <div>
+            <div className='pb-4'>
               <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
               <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
               <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
               <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
               <span className={styles.logoStar}><i className="bi bi-star-half"></i></span>
-              <span className={styles.desc}>(9.5)</span>
+              <span className={styles.desc}>(8.5)</span>
             </div>
             
             <div className={styles.h4Grey}>Price</div>
             <div className={styles.title}>$ 40.0</div>
+            <div className='pb-4' />
 
             <div className={styles.h4Black}>Color</div>
-            <div>
-              4Color
+            <div className={styles.productColorGroup}>
+              <div className={styles.productColor1}/>
+              <div className={styles.productColor2}/>
+              <div className={styles.productColor3}/>
+              <div className={styles.productColor4}/>
             </div>
+            <div className='pb-4' />
             
             <div className='row row-cols-auto'>
               <div className='col me-5'>
                 <div className={styles.h4Black}>Size</div>
+                <div className='pb-1' />
                 <div className='row row-cols-auto'>
                   <div className='col'>
-                    <i className="bi bi-dash-circle-fill"/>
+                    <i className="bi bi-dash-circle-fill" style={{color:'#9B9B9B'}}/>
                   </div>
                   <div className='col'>28</div>
                   <div className='col'>
-                    <i className="bi bi-plus-circle"/>
+                    <i className="bi bi-plus-circle" style={{color:'#9B9B9B'}}/>
                   </div>
                 </div>
               </div>
               <div className='col'>
                 <div className={styles.h4Black}>Jumlah</div>
+                <div className='pb-1' />
                 <div className='row row-cols-auto'>
                   <div className='col'>
-                    <i className="bi bi-dash-circle-fill"/>
+                    <i className="bi bi-dash-circle-fill" style={{color:'#9B9B9B'}}/>
                   </div>
                   <div className='col'>1</div>
                   <div className='col'>
-                    <i className="bi bi-plus-circle"/>
+                    <i className="bi bi-plus-circle" style={{color:'#9B9B9B'}}/>
                   </div>
                 </div>
               </div>
             </div>
+            <div className='pb-3' />
 
             <div className='row'>
               <div className='col-3'> <div className={styles.button}> <div className={styles.buttonCol3} type='button'>Chat</div> </div> </div>
@@ -160,70 +164,81 @@ function ProductDetail(props){
             </div>
           </div>
         </div>
+      </section>
         
-        <div>
-          <div className={styles.title}>Informasi Produk</div>
-          <div className={styles.h3}>Condition</div>
-          <div className={styles.h3red}>New</div>
-          <div className={styles.h3}>Description</div>
-          <div className={styles.desc}>Lorem </div>
-          <div className={styles.desc}>Lorem </div>
-          <div className={styles.desc}>Lorem </div>
-          <div className={styles.desc}>Lorem </div>
+      <section className='py-4'>
+        <div className={styles.title}>Informasi Produk</div>
+        <div className='pb-3'/>
+        <div className={styles.h3}>Condition</div>
+        <div className={styles.h3red}>New</div>
+        <div className='pb-3'/>
+        <div className={styles.h3}>Description</div>
+        <div className='pb-2'/>
+        <div className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        <div className='pb-2'/>
+        <div className={styles.desc}>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+        <div className='pb-2'/>
+        <div className={styles.desc}>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        <div className='pb-2'/>
+        <div className={styles.desc}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+        <div className='pb-2'/>
 
-          <div className={styles.title}>Product review </div>
-          <div className='row row-cols-auto'>
-            <div className='col'>
-              <div>
-                <span>9.5</span>  
-                <span>/10</span>  
-              </div>
-              <div>
-                <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
-                <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
-                <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
-                <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
-                <span className={styles.logoStar}><i className="bi bi-star-half"></i></span>
-              </div>
+        <div className='pt-4'/>
+        <div className={styles.title}>Product review </div>
+        <div className='pb-3'/>
+        <div className='row row-cols-auto'>
+
+          <div className='col'>
+            <div>
+              <span style={{fontSize:'50px', fontWeight:"bold"}}>8.5</span>  
+              <span style={{fontSize:'16px', color:'grey'}}>/10</span>  
             </div>
-            <div className='col'>
-              <div>
-                <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
-                <span>5</span>
-                <span>-----</span>
-                <span>4</span>
-              </div>
-              <div>
-                <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
-                <span>4</span>
-                <span>-----</span>
-                <span>0</span>
-              </div>
-              <div>
-                <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
-                <span>3</span>
-                <span>-----</span>
-                <span>0</span>
-              </div>
-              <div>
-                <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
-                <span>2</span>
-                <span>-----</span>
-                <span>0</span>
-              </div>
-              <div>
-                <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
-                <span>1</span>
-                <span>-----</span>
-                <span>0</span>
-              </div>
+            <div>
+              <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
+              <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
+              <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
+              <span className={styles.logoStar}><i className="bi bi-star-fill"></i></span>
+              <span className={styles.logoStar}><i className="bi bi-star-half"></i></span>
             </div>
-                
+          </div>
+          
+          <div className='col'>
+            <div className={styles.rating}>
+              <div className={styles.logoStar}><i className="bi bi-star-fill"></i></div>
+              <div className={styles.ratingValue}>5</div>
+              <div className={styles.ratingBar5}/>
+              <div className={styles.ratingValue}>5</div>
+            </div>
+            <div className={styles.rating}>
+              <div className={styles.logoStar}><i className="bi bi-star-fill"></i></div>
+              <div className={styles.ratingValue}>4</div>
+              <div className={styles.ratingBar4}/>
+              <div className={styles.ratingValue}>4</div>
+            </div>
+            <div className={styles.rating}>
+              <div className={styles.logoStar}><i className="bi bi-star-fill"></i></div>
+              <div className={styles.ratingValue}>3</div>
+              <div className={styles.ratingBar2}/>
+              <div className={styles.ratingValue}>2</div>
+            </div>
+            <div className={styles.rating}>
+              <div className={styles.logoStar}><i className="bi bi-star-fill"></i></div>
+              <div className={styles.ratingValue}>2</div>
+              <div className={styles.ratingBar0}/>
+              <div className={styles.ratingValue}>0</div>
+            </div>
+            <div className={styles.rating}>
+              <div className={styles.logoStar}><i className="bi bi-star-fill"></i></div>
+              <div className={styles.ratingValue}>1</div>
+              <div className={styles.ratingBar0}/>
+              <div className={styles.ratingValue}>0</div>
+            </div>
           </div>
 
         </div>
       </section>
-
+        
+      <hr/>
     </div>
   )
 }
