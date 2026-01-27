813c3e5d6c93168f1e09a0a6c13e53a364fa8fed
pitlimitCode <prabow14@gmail.com>
2026-01-27 10:05:14 +0700

    'quickCommit'

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,9 +1,9 @@
 
-export default function products(req, res) {
-  fetch('https://fakestoreapi.com/products')
-  .then(resp=>resp.json())
-  .then(json=>
-    res.status(200).json(json)
-  )
+export default async function products(req, res) {
+  await fetch('https://fakestoreapi.com/products')
+    .then(resp=>resp.json())
+    .then(json=>
+      res.status(200).json(json)
+    )
   // res.status(200).json({ message: 'Hello from Next.js API Route!' });
 }
